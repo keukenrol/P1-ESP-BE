2349a6ab553b444d9fab27a93b8d9ec7a85a3552
keukenrol <tim.vancauwenbergh@hotmail.com>
2023-08-18 11:03:40 +0200

    Update commons.h

--- ESP_P1/commons.h
+++ ESP_P1/commons.h
@@ -1,10 +1,12 @@
 /* EDIT FOLLOWING ITEMS ========================== */
 #define TYPE "T211"				// S211 or T211, functionality not yet implemented
+#define GAS 0
+#define WATER 0
 
 const char* ota_name = "ESP-P1";
 const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_PASS";
-const char* host = "XXX.XXX.XXX.XXX";
+const char* host = "192.168.0.20";
 const uint16_t port = 1889;
 
 #define REQ_PIN 0              // pin 0 -> ESP01S (GPIO0), ESP8266 (D3), ESP32 firebeetle (GPIO0)
@@ -17,10 +19,10 @@ const uint16_t port = 1889;
 #define IP_STATIC 1 // comment out if you want to use DHCP
 
 #if defined(IP_STATIC)
-IPAddress local_IP(XXX.XXX.XXX.XXX);
-IPAddress gateway(XXX.XXX.XXX.XXX);
-IPAddress subnet(XXX.XXX.XXX.XXX);
-IPAddress primaryDNS(XXX.XXX.XXX.XXX);
-IPAddress secondaryDNS(XXX.XXX.XXX.XXX);
+IPAddress local_IP(192,168,0,21);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+IPAddress primaryDNS(195,130,130,1);
+IPAddress secondaryDNS(195,130,131,1);
 #endif
 /* END EDIT ITEMS ========================== */
